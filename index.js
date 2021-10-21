6d1ddd95280a63e40cdbb9125af1a123e3553c9a
Randolf Lapina <randolflapina26@gmail.com>
2021-10-21 10:00:31 +0800

    test issue

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/messenger', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/html/index.html');
 });
 
